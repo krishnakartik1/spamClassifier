3007c1816d135fa13bb4ff724ea60330ceccf1af
krishnakartik1 <krishnakartik1@gmail.com>
2016-12-06 21:11:54 +0530

    first commit

---
+++ spamClassifier.py
@@ -0,0 +1,17 @@
+from sklearn import datasets
+iris = datasets.load_iris()
+x=iris.data
+y=iris.target
+
+from sklearn.cross_validation import train_test_split 
+xTrain, xTest,yTrain,yTest = train_test_split(x,y,test_size = 0.5)
+
+from sklearn import tree
+myClassifier=tree.DecisionTreeClassifier()
+myClassifier.fit(xTrain,yTrain)
+predictions = myClassifier.predict(xTest)
+print predictions
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(yTest,predictions)
+
